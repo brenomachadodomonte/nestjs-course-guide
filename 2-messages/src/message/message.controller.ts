5650812bb5298fba86a89fc80b79075cc25e342a
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-01-14 21:47:00 -0300

    Creating the get all messages request

--- 2-messages/src/message/message.controller.ts
+++ 2-messages/src/message/message.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+
+    @Get()
+    listMessages() {
+        return { message: 'GET MESSAGES' };
+    }
+    
+}
