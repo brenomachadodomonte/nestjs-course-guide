76933b70039bc6a4779bbd878404c2048b4ce435
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-01-18 19:40:00 -0300

    Implementing findall method

--- 2-messages/src/message/message.repository.ts
+++ 2-messages/src/message/message.repository.ts
@@ -10,11 +10,12 @@ export class MessageRepository {
     }
 
     async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
 
+        return messages;
     }
 
-    async create(message: string) {
-
-    }
+    async create(message: string) {}
 
 }
