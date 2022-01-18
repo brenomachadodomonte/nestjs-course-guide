b1cf4b840fd0c4b279a4aa65bad6e30788cdd19f
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-01-18 19:49:00 -0300

    Creating message service

---
+++ 2-messages/src/message/message.service.ts
@@ -0,0 +1,24 @@
+import { MessageRepository } from "./message.repository"
+
+
+export class MessageService {
+
+    messagesRepo: MessageRepository;
+
+    constructor() {
+        // Don't do this on real apps
+        this.messagesRepo = new MessageRepository();
+    }
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    create(message: string) {
+        return this.create(message);
+    }
+}
