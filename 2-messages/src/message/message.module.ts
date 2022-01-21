f482eafbf792171a646871aa4cbf9a106453e616
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-01-21 19:01:00 -0300

    providing message service and repository

--- 2-messages/src/message/message.module.ts
+++ 2-messages/src/message/message.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MessageController } from './message.controller';
+import { MessageRepository } from './message.repository';
+import { MessageService } from './message.service';
 
 @Module({
-  controllers: [MessageController]
+  controllers: [MessageController],
+  providers: [
+    MessageService,
+    MessageRepository
+  ]
 })
 export class MessageModule {}
