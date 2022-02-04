e1dab581a646f909d0b197fa64d781d91cd734cd
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-04 14:37:00 -0300

    Importing database in app module

--- 4-mycarvalue/src/app.module.ts
+++ 4-mycarvalue/src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ReportModule],
+  imports: [
+    UserModule, 
+    ReportModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
