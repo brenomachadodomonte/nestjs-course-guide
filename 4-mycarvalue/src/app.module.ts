a41c1b33ce33c69a4482ed2ae4be2b99175a2dd3
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-04 14:30:00 -0300

    creating report module

--- 4-mycarvalue/src/app.module.ts
+++ 4-mycarvalue/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
