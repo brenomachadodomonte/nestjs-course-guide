14f9c593fc95e7c82bf404cfff6c7be067200718
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-05 18:35:00 -0300

    Creating create user endpoint

--- 4-mycarvalue/src/user/user.controller.ts
+++ 4-mycarvalue/src/user/user.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+
+    constructor(private service: UserService){}
 
     @Post('/signup')
     createUser(
         @Body() body: CreateUserDto
     ){
-        console.log(body);
+        return this.service.create(body);
     }
 
 }
--- 4-mycarvalue/src/user/user.service.ts
+++ 4-mycarvalue/src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private repo: Repository<User>
+    ){}
+
+    create(dto: CreateUserDto) {
+        const { email, password } = dto;
+        const user = this.repo.create({ email, password });
+
+        return this.repo.save(user);
+    }
+
+
+}
