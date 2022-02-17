44a9d0aadfd84eb3417f9916dff5588d999c66db
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-17 19:18:00 -0300

    Creating auth service

--- 4-mycarvalue/src/user/user.module.ts
+++ 4-mycarvalue/src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from './auth.service';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AuthService],
   imports: [
     TypeOrmModule.forFeature([User])
   ]
