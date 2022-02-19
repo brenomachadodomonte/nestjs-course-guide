c84fbcf36eb42b805b208c2da24760cf947e6259
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-19 10:07:00 -0300

    Setting current user interceptor as global

--- 4-mycarvalue/src/user/user.module.ts
+++ 4-mycarvalue/src/user/user.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { CurrentUserInteceptor } from './interceptors/current-user.interceptor';
@@ -8,7 +9,14 @@ import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, AuthService, CurrentUserInteceptor],
+  providers: [
+    UserService, 
+    AuthService, 
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInteceptor
+    }
+  ],
   imports: [
     TypeOrmModule.forFeature([User])
   ]
