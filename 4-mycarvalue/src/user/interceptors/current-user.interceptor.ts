756763ac57acd7a9669454459fe7117d7412f291
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-02-19 10:01:00 -0300

    Creating current user decorator and interceptor

---
+++ 4-mycarvalue/src/user/interceptors/current-user.interceptor.ts
@@ -0,0 +1,21 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { UserService } from "../user.service";
+
+@Injectable()
+export class CurrentUserInteceptor implements NestInterceptor {
+
+    constructor(private userService: UserService){}
+
+    async intercept(context: ExecutionContext, next: CallHandler) {
+        const request = context.switchToHttp().getRequest();
+        const { userId } = request.session || {};;
+
+        if(userId) {
+            const user = await this.userService.findOne(userId);
+            request.currentUser = user;
+        }
+
+        return next.handle();
+    }
+}
