8cb1ac8a83adcf6e976f45a472ca59b7ff974a63
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-10-18 20:34:29 -0300

    Creating the signup method

--- 4-mycarvalue/src/user/auth.service.ts
+++ 4-mycarvalue/src/user/auth.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { randomBytes, scrypt as _script } from "crypto";
+import { promisify } from "util";
+import { CreateUserDto } from "./dto/create-user.dto";
 
+const scrypt = promisify(_script);
 
 @Injectable()
 export class AuthService {
+
+    constructor(private userService: UserService){}
+
+    async signup(email: string, password: string) {
+        const users = await this.userService.find(email);
+
+        if(users.length) {
+            throw new BadRequestException('Email in use');
+        }
+
+        //Generate a salt
+        const salt = randomBytes(8).toString('hex');
+
+        //hash the salt and the passoword together
+        const hash = await scrypt(password, salt, 32) as Buffer;
+
+        //join the hashed result and salt together
+        const result = salt + '.' + hash.toString('hex');
+        
+        const user = await this.userService.create({email, password});
+
+        return user;
+    }
+
+    signin() {
+        
+    }
 
 }
